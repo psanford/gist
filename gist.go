71e834e13f0b49f13af1c602fe805d7f1ce45cb6
Peter Sanford <peter@sanford.io>
2016-08-19 07:59:40 -0700

    Fix copyright and add license.
    My editor is overly aggressive with adding my company's copyright notice.

--- gist.go
+++ gist.go
@@ -1,9 +1,4 @@
-// Copyright (c) 2015, RetailNext, Inc.
-// This material contains trade secrets and confidential information of
-// RetailNext, Inc.  Any use, reproduction, disclosure or dissemination
-// is strictly prohibited without the explicit written permission
-// of RetailNext, Inc.
-// All rights reserved.
+// Copyright (c) 2015 Peter Sanford
 
 package main
 
